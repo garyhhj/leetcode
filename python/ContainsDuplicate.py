bb2b279e662bfbe1b8e14516b09b6c88e1f8fc0f
Gary Huang <gary_1327@yahoo.com>
2023-08-22 19:54:55 -0400

    solution for valid anagram

--- python/ContainsDuplicate.py
+++ python/ContainsDuplicate.py
@@ -1,6 +1,5 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        #we can use a map and map numbers and check the output of the map 
         freq = {}
         for i in nums: 
             if i in freq: 
---
+++ python/ValidAnagram.py
@@ -0,0 +1,10 @@
+def getFreq(freq: {}, s: str) -> None: 
+    for i in s: 
+        freq[i] = 1 + freq.get(i, 0)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freqS, freqT = {}, {}
+        getFreq(freqS, s)
+        getFreq(freqT, t)
+        return freqS == freqT
