d9461d6635b72b7a504a7808e64539ab8b63d52c
Gary Huang <gary_1327@yahoo.com>
2023-08-25 22:50:17 -0400

    sol product of array except itself

--- python/ProductOfArrayExceptSelf.py
+++ python/ProductOfArrayExceptSelf.py
@@ -1,21 +1,15 @@
-#leetcode #238
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        r = [1] * len(nums)
-        l = [1] * len(nums)
+        ans = [1] * len(nums); 
 
-        l[0] = nums[0]
+        prev = nums[0]
         for i in range(1, len(nums), 1): 
-            l[i] = nums[i] * l[i - 1]
+            ans[i] *= prev
+            prev *= nums[i]
 
-        
-        r[len(nums) - 1]  = nums[len(nums) - 1]
+        nxt = nums[len(nums) - 1]    
         for i in range(len(nums) - 2, -1, -1): 
-            r[i] = nums[i] * r[i + 1]
+            ans[i] *= nxt
+            nxt *= nums[i]
         
-        ans = [1] * len(nums)
-        for i in range(0, len(nums), 1): 
-            ans[i] = (l[i - 1] if i - 1 >= 0 else 1) * \
-            (r[i + 1] if (i + 1) < len(nums) else 1) 
-
         return ans
