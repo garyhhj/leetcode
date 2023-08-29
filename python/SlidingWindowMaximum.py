3d65f4e91d9509febc895f4bd09e55d8a3f639f6
Gary Huang <gary_1327@yahoo.com>
2023-08-29 19:06:09 -0400

    solution for sliding window maximum

--- python/SlidingWindowMaximum.py
+++ python/SlidingWindowMaximum.py
@@ -1,40 +1,27 @@
-#leetcode #239
+#leetcode #239 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        #use a segment tree for (n + k)logn time complexity and O(n) space 
-
-        #initializing segment tree 
-        segmentTree = [0] * (2 * len(nums))
-        for i in range(len(nums), len(segmentTree), 1): 
-            segmentTree[i] = nums[i - len(nums)]
-        for  i in range(len(nums) - 1, 0, -1): 
-            segmentTree[i] = -inf 
         
-        for i in range(len(segmentTree) - 1, 0, -1): 
-            segmentTree[i // 2] = max(segmentTree[i // 2], segmentTree[i])
-
-        # search range [l, r)
-        def query(l : int, r : int): 
-            l += len(nums)
-            r += len(nums)
-            res = -inf 
-            while l < r:
-                if l % 2 == 1: 
-                    res = max(res, segmentTree[l])
-                    l += 1
-                if r % 2 == 1: 
-                    r -= 1
-                    res = max(res, segmentTree[r])
-
-                l //= 2
-                r //= 2 
-            return res 
-
-        #range query 
         res = []
-        for i in range(0, len(nums) - k + 1, 1): 
-            res.append(query(i, i + k))
-        return res 
+        que = collections.deque()
+
+        for i in range(0, k, 1): 
+            while len(que) > 0 and que[-1] < nums[i]: 
+                que.pop()
+            que.append(nums[i])
+            
+        for i in range(0, len(nums) - k, 1):
+            #add max of que to result 
+            res.append(que[0])
+            #remove old element 
+            if que[0] == nums[i]: 
+                que.popleft()
+
+            #remove smaller elements in que 
+            while len(que) > 0 and que[-1] < nums[i + k]: 
+                que.pop()
+            que.append(nums[i + k])
 
 
-
+        res.append(que[0])
+        return res
