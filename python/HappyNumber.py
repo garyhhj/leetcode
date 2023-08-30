c694f0fd8a64f0102c058eb7f90017122239f49e
Gary Huang <gary_1327@yahoo.com>
2023-08-30 19:02:30 -0400

    solution for happy number

--- python/HappyNumber.py
+++ python/HappyNumber.py
@@ -1,17 +1,20 @@
 #leetcode #202
+def nextState(n : int) -> int: 
+    temp = 0
+    while n != 0:
+        temp += (n % 10) ** 2 
+        n //= 10
+    n = temp
+    return temp
+
 class Solution:
     def isHappy(self, n: int) -> bool:
-        visited = {}
+        slow, fast = n, nextState(n)
 
-        while n != 1:
-            if n in visited: 
-                return False
-            visited[n] = True 
+        while slow != fast: 
+            slow = nextState(slow)
+            fast = nextState(fast)
+            fast = nextState(fast)
+        
+        return True if fast == 1 else False 
 
-            temp = 0
-            while n != 0: 
-                temp += (n % 10) * (n % 10)
-                n //= 10
-            n = temp
-
-        return True 
