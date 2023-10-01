9e2dec1480bb6eb0342ce8a19b3a48c27ffc9b30
Gary Huang <gary_1327@yahoo.com>
2023-10-01 16:04:56 -0400

    solution for leetocde #2873

---
+++ python/MaxValueOfOrderedTripletOne.py
@@ -0,0 +1,11 @@
+#leetcode #2873
+
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        res = 0
+        for i in range(0, len(nums), 1): 
+            for j in range(i + 1, len(nums), 1): 
+                for k in range(j + 1, len(nums), 1): 
+                    res = max(res, (nums[i] - nums[j]) * nums[k])      
+        return res
+    
