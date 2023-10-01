1ce6a5cae310610f5ec80a68fbe8387e00e673de
Gary Huang <gary_1327@yahoo.com>
2023-10-01 15:39:03 -0400

    solution for leetcode #557

---
+++ python/ReverseWordsInStringThree.py
@@ -0,0 +1,14 @@
+#leetcode #557
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        res = ""
+        curr = ""
+        for i in range(0, len(s), 1): 
+            if s[i] == " ": 
+                res += curr[::-1]
+                curr = ""
+                res += " "
+                continue
+            curr +=  s[i]
+        return res + curr[::-1]
